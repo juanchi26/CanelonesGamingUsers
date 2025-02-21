01dd844bfd4aa451add42241ddd37626b1a11428
Juan Gabriel Díaz <juanchi.diaz@hotmail.com>
2025-02-21 18:36:40 -0300

    Ultimo Commit

--- subo.bat
+++ subo.bat
@@ -3,7 +3,6 @@ git commit -m "Ultimo Commit"
 git push
 set GOOS=linux
 set GOARCH=amd64
-
-go build main.go
-del main.zip
-tar.exe -a -cf main.zip main
+go build -o bootstrap main.go
+del bootstrap.zip
+tar.exe -a -cf bootstrap.zip bootstrap
